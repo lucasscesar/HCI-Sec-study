6add781a79db5c3b71125b6ccf25ccf44cb59072
Lucas.Ferreira <lucascesarf@gmail.com>
2018-02-22 00:34:32 -0300

    last

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
-$('header a').click( function(){
-	var id = $(this).attr('href');
+jQuery(document).ready(function($) {
+	$("#fig4 img").elevateZoom({
+	  zoomType				: "inner",
+	  cursor: "crosshair"
+	});
 
-	//console.log($(id));
-	$("html, body").animate({ scrollTop: ( $(id).offset().top - 80 ) }, "slow");
-	return false;
+	$('header a').click( function(){
+		var id = $(this).attr('href');
+
+		//console.log($(id));
+		$("html, body").animate({ scrollTop: ( $(id).offset().top - 80 ) }, "slow");
+		return false;
+	});
+
 });
+
+
+
